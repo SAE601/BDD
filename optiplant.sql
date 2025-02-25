c692a655f609617acaa39e3f0a165fffe2d0313f
HeliosFz <144106354+HeliosFz@users.noreply.github.com>
2025-02-25 14:59:02 +0100

    Update optiplant.sql

--- optiplant.sql
+++ optiplant.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : mar. 25 fév. 2025 à 11:21
--- Version du serveur : 9.1.0
--- Version de PHP : 8.3.14
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 25, 2025 at 01:58 PM
+-- Server version: 9.1.0
+-- PHP Version: 8.3.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,48 +18,47 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `optiplant`
+-- Database: `optiplant`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `actionneurs`
+-- Table structure for table `actionneurs`
 --
 
 DROP TABLE IF EXISTS `actionneurs`;
 CREATE TABLE IF NOT EXISTS `actionneurs` (
   `idActionneur` int NOT NULL AUTO_INCREMENT,
   `typeActionneur` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `localisation` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `dateInstallation` date DEFAULT NULL,
   `idBac` int DEFAULT NULL,
   `datemaintenance` date DEFAULT NULL,
   PRIMARY KEY (`idActionneur`),
   KEY `idBac` (`idBac`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `alertes`
+-- Table structure for table `alertes`
 --
 
 DROP TABLE IF EXISTS `alertes`;
 CREATE TABLE IF NOT EXISTS `alertes` (
   `idAlerte` int NOT NULL AUTO_INCREMENT,
   `typeAlerte` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `dateHeure` datetime NOT NULL,
+  `dateHeure` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `message` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   `idBac` int DEFAULT NULL,
   PRIMARY KEY (`idAlerte`),
   KEY `idBac` (`idBac`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `bacs`
+-- Table structure for table `bacs`
 --
 
 DROP TABLE IF EXISTS `bacs`;
@@ -72,12 +71,12 @@ CREATE TABLE IF NOT EXISTS `bacs` (
   `energieconsommee` int DEFAULT NULL,
   PRIMARY KEY (`idBac`),
   KEY `idPlante` (`idPlante`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `capteurs`
+-- Table structure for table `capteurs`
 --
 
 DROP TABLE IF EXISTS `capteurs`;
@@ -90,25 +89,42 @@ CREATE TABLE IF NOT EXISTS `capteurs` (
   `datemaintenance` date DEFAULT NULL,
   PRIMARY KEY (`idCapteur`),
   KEY `idBac` (`idBac`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `groupe`
+-- Table structure for table `groupe`
 --
 
 DROP TABLE IF EXISTS `groupe`;
 CREATE TABLE IF NOT EXISTS `groupe` (
   `idGroupe` int NOT NULL AUTO_INCREMENT,
-  `nom` varchar(20) COLLATE utf32_unicode_ci NOT NULL,
+  `nom` varchar(20) CHARACTER SET utf32 COLLATE utf32_unicode_ci NOT NULL,
   PRIMARY KEY (`idGroupe`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `meteo`
+-- Table structure for table `irrigation`
+--
+
+DROP TABLE IF EXISTS `irrigation`;
+CREATE TABLE IF NOT EXISTS `irrigation` (
+  `idIrrigation` int NOT NULL AUTO_INCREMENT,
+  `dateHeure` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `idBac` int DEFAULT NULL,
+  `idRecette` int DEFAULT NULL,
+  PRIMARY KEY (`idIrrigation`),
+  KEY `contrainteirig1` (`idBac`),
+  KEY `contrainteirig2` (`idRecette`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meteo`
 --
 
 DROP TABLE IF EXISTS `meteo`;
@@ -121,12 +137,12 @@ CREATE TABLE IF NOT EXISTS `meteo` (
   `pluie` tinyint(1) DEFAULT NULL,
   `vent` int DEFAULT NULL,
   PRIMARY KEY (`idMeteo`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `ombrieres`
+-- Table structure for table `ombrieres`
 --
 
 DROP TABLE IF EXISTS `ombrieres`;
@@ -136,25 +152,25 @@ CREATE TABLE IF NOT EXISTS `ombrieres` (
   `modeFonctionnement` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `dateHeure` datetime NOT NULL,
   PRIMARY KEY (`idOmbriere`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `periode`
+-- Table structure for table `periode`
 --
 
 DROP TABLE IF EXISTS `periode`;
 CREATE TABLE IF NOT EXISTS `periode` (
   `idPeriode` int NOT NULL AUTO_INCREMENT,
-  `nom` varchar(20) COLLATE utf32_unicode_ci NOT NULL,
+  `nom` varchar(20) CHARACTER SET utf32 COLLATE utf32_unicode_ci NOT NULL,
   PRIMARY KEY (`idPeriode`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `plantes`
+-- Table structure for table `plantes`
 --
 
 DROP TABLE IF EXISTS `plantes`;
@@ -167,12 +183,12 @@ CREATE TABLE IF NOT EXISTS `plantes` (
   PRIMARY KEY (`idPlante`),
   KEY `idGroupe` (`idGroupe`),
   KEY `idPeriode` (`idPeriode`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `productionelectrique`
+-- Table structure for table `productionelectrique`
 --
 
 DROP TABLE IF EXISTS `productionelectrique`;
@@ -181,20 +197,22 @@ CREATE TABLE IF NOT EXISTS `productionelectrique` (
   `dateHeure` datetime NOT NULL,
   `energieProduite` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`idProduction`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `recettes`
+-- Table structure for table `recettes`
 --
 
 DROP TABLE IF EXISTS `recettes`;
 CREATE TABLE IF NOT EXISTS `recettes` (
+  `idRecette` int NOT NULL AUTO_INCREMENT,
   `idGroupe` int NOT NULL,
   `idPeriode` int NOT NULL,
   `eauArrosage` decimal(5,2) DEFAULT NULL,
-  `arrosage/jour` decimal(5,2) DEFAULT NULL,
+  `arrosage_jour` decimal(5,2) DEFAULT NULL,
+  `quotidien` tinyint NOT NULL,
   `eauTotalJour` decimal(5,2) DEFAULT NULL,
   `rationNPK` int DEFAULT NULL,
   `azote` decimal(4,2) DEFAULT NULL,
@@ -205,10 +223,59 @@ CREATE TABLE IF NOT EXISTS `recettes` (
   `tempsActivationPotassium` decimal(4,2) DEFAULT NULL,
   `quantiteNutriment` decimal(4,2) DEFAULT NULL,
   `seuilHumidite` decimal(5,2) DEFAULT NULL,
-  PRIMARY KEY (`idGroupe`,`idPeriode`) USING BTREE,
+  PRIMARY KEY (`idRecette`),
   KEY `idGroupe` (`idGroupe`),
   KEY `idPeriode` (`idPeriode`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `actionneurs`
+--
+ALTER TABLE `actionneurs`
+  ADD CONSTRAINT `contrainteBac` FOREIGN KEY (`idBac`) REFERENCES `bacs` (`idBac`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `alertes`
+--
+ALTER TABLE `alertes`
+  ADD CONSTRAINT `contrainteAlertes` FOREIGN KEY (`idBac`) REFERENCES `bacs` (`idBac`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `bacs`
+--
+ALTER TABLE `bacs`
+  ADD CONSTRAINT `contrainte3` FOREIGN KEY (`idPlante`) REFERENCES `plantes` (`idPlante`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `capteurs`
+--
+ALTER TABLE `capteurs`
+  ADD CONSTRAINT `contraintecapteurs` FOREIGN KEY (`idBac`) REFERENCES `bacs` (`idBac`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `irrigation`
+--
+ALTER TABLE `irrigation`
+  ADD CONSTRAINT `contrainteirig1` FOREIGN KEY (`idBac`) REFERENCES `bacs` (`idBac`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `contrainteirig2` FOREIGN KEY (`idRecette`) REFERENCES `recettes` (`idRecette`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `plantes`
+--
+ALTER TABLE `plantes`
+  ADD CONSTRAINT `contrainte01` FOREIGN KEY (`idGroupe`) REFERENCES `groupe` (`idGroupe`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `contrainte02` FOREIGN KEY (`idPeriode`) REFERENCES `periode` (`idPeriode`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `recettes`
+--
+ALTER TABLE `recettes`
+  ADD CONSTRAINT `contrainte1` FOREIGN KEY (`idGroupe`) REFERENCES `groupe` (`idGroupe`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `contrainte2` FOREIGN KEY (`idPeriode`) REFERENCES `periode` (`idPeriode`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

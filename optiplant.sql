923e6283581652a893aa0d46e0fd33ab440cab6e
AZIZTaha83 <144880789+AZIZTaha83@users.noreply.github.com>
2025-02-28 14:11:02 +0100

    Update optiplant.sql

--- optiplant.sql
+++ optiplant.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 28 fév. 2025 à 13:06
+-- Généré le : ven. 28 fév. 2025 à 13:10
 -- Version du serveur : 9.1.0
 -- Version de PHP : 8.3.14
 
@@ -383,4 +383,3 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
